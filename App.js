2bb97cd3480a1f4d5592f518e876f3071a36f8b7
Herbi Tarik <herbitarik2509@gmail.com>
2023-04-25 18:24:54 +0200

    Installing react-native-maps and adding MapView

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import MapView, { Marker, Polyline } from 'react-native-maps'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [origin, setOrigin] = useState({
+        latitude: 33.640411,
+        longitude: -84.419853,
+    })
+    const [destination, setDestination] = useState({
+        latitude: 33.640411,
+        longitude: -84.419853,
+    })
+
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: origin.latitude,
+                    longitude: origin.longitude,
+                    latitudeDelta: 0.09,
+                    longitudeDelta: 0.04,
+                }}
+            >
+                <Marker
+                    draggable
+                    coordinate={origin}
+                    onDragEnd={(direction) =>
+                        setOrigin(direction.nativeEvent.coordinate)
+                    }
+                />
+                <Marker
+                    draggable
+                    coordinate={destination}
+                    onDragEnd={(direction) =>
+                        setOrigin(direction.nativeEvent.coordinate)
+                    }
+                />
+                <Polyline coordinates={[origin, destination]} />
+            </MapView>
+
+            <StatusBar style="auto" />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        marginBottom: 'auto',
+        width: '100%',
+        height: '100%',
+    },
+})
